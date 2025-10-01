e7c5e54cf184491bb483a285a0eb3b46ba51c852
nuulun <bekovn91@gmail.com>
2025-10-01 12:15:57 +0500

    feat(mergesort): baseline + reuse buffer + cutoff + tests

--- src/test/java/algo/MergeSortTest.java
+++ src/test/java/algo/MergeSortTest.java
@@ -9,15 +9,27 @@ public class MergeSortTest {
     void testMetrics() {
         int n = 1000;
         int trials = 100;
-        for (int t = 0; t < trials/3+1; t++) {
+        for (int t = 0; t < trials/6+1; t++) {
            test(n);
         }
+        n = 5000;
+        for (int t = 0; t < trials/6+1; t++) {
+            test(n);
+        }
         n = 10000;
-        for (int t = 0; t < trials/3+1; t++) {
+        for (int t = 0; t < trials/6+1; t++) {
+            test(n);
+        }
+        n = 20000:
+        for (int t = 0; t < trials/6+1; t++) {
+            test(n);
+        }
+        n = 50000;
+        for (int t = 0; t < trials/6+1; t++) {
             test(n);
         }
         n = 100000;
-        for (int t = 0; t < trials/3+1; t++) {
+        for (int t = 0; t < trials/6+1; t++) {
             test(n);
         }
     }
@@ -30,6 +42,7 @@ public class MergeSortTest {
         MergeSort.sort(arr, metrics);
         long end = System.nanoTime();
 
+        System.out.println("Algorithm       : " + "MergeSort");
         System.out.println("Array size      : " + n);
         System.out.println("Time (ms)       : " + (end - start) / 1_000_000.0);
         System.out.println("Comparisons     : " + metrics.comparisons);
